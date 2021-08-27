7c30149527dfc1997dc0fd096b7043c2ca71887a
Caleb Wittman <calebjwittman1991@gmail.com>
2021-08-27 14:27:28 -0600

    Implement queries for users in graphql

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # gem 'rack-cors'
 gem 'graphql'
 gem 'graphiql-rails'
+gem 'pry'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,33 +1,23 @@
 module Types
   class QueryType < Types::BaseObject
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    # include GraphQL::Types::Relay::HasNodeField
+    # include GraphQL::Types::Relay::HasNodesField
 
-    field :users, Types::UsersType, null: false do
-      argument :id, ID!, required: true
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
     end
 
-    def users(id:)
+    def user(id:)
       User.find(id)
     end
 
     field :characters, Types::CharacterType, null: false do
-      argument :id, ID!, required: true
+      argument :id, ID, required: true
     end
 
     def characters(id:)
       Character.find(id)
-    end
-
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
     end
   end
 end
