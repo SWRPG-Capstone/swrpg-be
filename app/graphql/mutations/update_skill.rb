d5c976bb0fbab0f90f390424674ec77449b7f06d
Suzie K <suziekim.dev@gmail.com>
2021-09-01 09:32:52 -0400

    Comment our update skill mutation

--- app/graphql/mutations/update_skill.rb
+++ app/graphql/mutations/update_skill.rb
@@ -1,12 +1,12 @@
-class Mutation::UpdateSkill < Mutations::BaseMutation
+# class Mutation::UpdateSkill < Mutations::BaseMutation
 
-  # argument
-  #
-  # field
+#   # argument
+#   #
+#   # field
 
-  def resolve(character_id:, skill:, amount_changed:)
-    character = Character.find(character_id)
-    skill_current_level = character.skills.skill
-    update = character.skills.update(skill: (skill_current_level += amount_changed)
-  end
-end
+#   def resolve(character_id:, skill:, amount_changed:)
+#     character = Character.find(character_id)
+#     skill_current_level = character.skills.skill
+#     update = character.skills.update(skill: (skill_current_level += amount_changed)
+#   end
+# end
