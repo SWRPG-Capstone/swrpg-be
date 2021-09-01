6daf12751e8c4b6ff2fb6ce3dee0982382de768d
Suzie K <suziekim.dev@gmail.com>
2021-08-31 21:01:03 -0400

    Add validations

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe Character, type: :model do
   describe 'relationships' do
     it { should belong_to(:user) }
     it { should have_many(:characteristics) }
-    it { should have_many(:skills) } 
-    it { should have_many(:obligations) } 
-    it { should have_many(:critical_injuries) } 
-    it { should have_many(:equipment_log) } 
+    it { should have_many(:skills) }
+    it { should have_many(:obligations) }
+    it { should have_many(:critical_injuries) }
+    it { should have_many(:equipment_log) }
     it { should have_many(:motivations) }
     it { should have_many(:talents) }
   end
