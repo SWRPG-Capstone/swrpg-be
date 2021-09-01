1f4f6572569dcb2ce15e9417ea9d08742c4ff9d8
Suzie K <suziekim.dev@gmail.com>
2021-09-01 09:25:52 -0400

    Update User Mutations

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -1,22 +1,27 @@
-class Mutation::CreateUser < Mutations::BaseMutation
-  argument :username, String, required: true
+module Mutations 
+  module Users 
+    class CreateUser < ::Mutations::BaseMutation
+      argument :username, String, required: true
 
-  field :user, Types::UserType, null: false
-  field :errors, [String], null:false
+      field :user, Types::UserType, null: false
+      field :errors, [String], null:false
 
-  def resolve(username:)
-    user = User.create(username: username)
+      def resolve(username:)
+        user = User.create(username: username)
 
-    if user.save
-      {
-        user: user,
-        errors: []
-      }
-    else
-      {
-        user: nil,
-        errors: [user.errors.full_messages]
-      }
+        if user.save
+          {
+            user: user,
+            errors: []
+          }
+        else
+          {
+            user: nil,
+            errors: [user.errors.full_messages]
+          }
+        end
+      end
     end
   end
 end
+
