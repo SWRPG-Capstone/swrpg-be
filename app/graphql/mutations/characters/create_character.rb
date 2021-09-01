45703d01b8fe0c09161ae8d0ac99e0c58c419a0c
Suzie K <suziekim.dev@gmail.com>
2021-08-31 21:02:13 -0400

    Green test create character

---
+++ app/graphql/mutations/characters/create_character.rb
@@ -0,0 +1,31 @@
+module Mutations
+  module Characters
+    class CreateCharacter < ::Mutations::BaseMutation
+      argument :user_id, String, required: true
+      argument :name, String, required: true
+      argument :species, String, required: true
+      argument :specialization, String, required: true
+      argument :career, String, required: true
+      argument :age, Integer, required: true
+      argument :height, String, required: true
+      argument :build, String, required: true
+      argument :hair, String, required: true
+      argument :eyes, String, required: true
+      type Types::CharacterType
+    
+      def resolve(user_id:, name:, species:, specialization:, career:, age:, height:, build:, hair:, eyes:)
+        
+        character = Character.new(user_id: user_id, name: name, species: species, specialization: specialization, career: career, age: age, height: height, build: build, hair: hair, eyes: eyes)
+        
+        if character.save
+          character
+        else
+          {
+            character: nil,
+            errors: [character.errors.full_messages]
+          }
+        end
+      end
+    end
+  end
+end
