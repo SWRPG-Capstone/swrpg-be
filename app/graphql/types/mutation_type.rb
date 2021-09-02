a27d9fbe081fd309837ce24e838f20545c03d38b
Joe Ray <jray100.jr@gmail.com>
2021-09-02 15:10:07 -0600

    Add field for update skill

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_character, mutation: Mutations::Characters::CreateCharacter
+    field :update_skill, mutation: Mutations::Skills::UpdateSkill
   end
 end
