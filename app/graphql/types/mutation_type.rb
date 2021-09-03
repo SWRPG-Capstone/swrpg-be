70e0bfbdd90f1c915fa45ce4712221fb42847d9a
Caleb1991 <calebjwittman1991@gmail.com>
2021-09-02 19:07:16 -0600

    Create skill mutation, and accompanying test

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_character, mutation: Mutations::Characters::CreateCharacter
+    field :create_skill, mutation: Mutations::Skills::CreateSkill
   end
 end
