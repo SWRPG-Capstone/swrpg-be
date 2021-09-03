11b4cc8013f26d59d066e36429212c4fd73dfd85
Suzie K <suziekim.dev@gmail.com>
2021-09-03 17:42:18 -0400

    Update create character test

--- spec/graphql/mutations/characters/create_character_spec.rb
+++ spec/graphql/mutations/characters/create_character_spec.rb
@@ -6,32 +6,32 @@ RSpec.describe 'CreateCharacter', type: :request do
       @user = User.create(username: "test")
       @query = <<~GQL
                     mutation {
-                      character: createCharacter(
-                                              input: {
-                                                userId: "#{@user.id}"
-                                                name: "Mace Windu"
-                                                species: "human"
-                                                specialization: "being very serious"
-                                                career: "jedi"
-                                                age: 34
-                                                height: "6ft 5in"
-                                                build: "lean"
-                                                hair: "bald"
-                                                eyes: "black"
-                                            } 
-                                            ) {
-                                              age 
-                                              build
-                                              career 
-                                              eyes 
-                                              hair
-                                              height 
-                                              id 
-                                              name
-                                              specialization
-                                              species
-                                            }
-                                          }
+                      createCharacter(
+                                      input: {
+                                      userId: "#{@user.id}"
+                                      name: "Mace Windu"
+                                      species: "human"
+                                      specialization: "being very serious"
+                                      career: "jedi"
+                                      age: 34
+                                      height: "6ft 5in"
+                                      build: "lean"
+                                      hair: "bald"
+                                      eyes: "black"
+                                   } 
+                                 ) {
+                                      age 
+                                      build
+                                      career 
+                                      eyes 
+                                      hair
+                                      height 
+                                      id 
+                                      name
+                                      specialization
+                                      species
+                                    }
+                                  }
               GQL
     end
 
