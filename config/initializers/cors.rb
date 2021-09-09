49f52338c249a0f8e0dd61b42642be5646a1e009
Suzie K <suziekim.dev@gmail.com>
2021-09-08 23:00:24 -0400

    Update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins 'localhost:3000', 'https://swrpg-companion.herokuapp.com/'
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :patch, :put]
   end
 end
