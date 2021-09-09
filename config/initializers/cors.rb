7821db00991c3ce1e1aad36651cf84a4fff02bed
Suzie K <suziekim.dev@gmail.com>
2021-09-08 23:04:03 -0400

    Remove http protocol from live app domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://swrpg-companion.herokuapp.com/'
+    origins 'localhost:3000', 'swrpg-companion.herokuapp.com'
     resource '*', 
       headers: :any, 
       methods: [:get, :post, :patch, :put]
