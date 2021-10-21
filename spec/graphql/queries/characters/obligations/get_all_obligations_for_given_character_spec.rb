da28a192cb79bbf98b56859fb0cc84ed97b8d75f
Suzie K <suziekim.dev@gmail.com>
2021-10-21 15:18:09 -0400

    [Refactor] Fix typo

--- spec/graphql/queries/characters/obligations/get_all_obligations_for_given_character_spec.rb
+++ spec/graphql/queries/characters/obligations/get_all_obligations_for_given_character_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Types::CharacteristicType, type: :request do
-  describe 'display characteristics' do
+RSpec.describe Types::ObligationType, type: :request do
+  describe 'display obligations' do
     describe 'happy path' do
-      it 'returns a characters characteristics' do
+      it 'returns a characters obligations' do
         user = User.create(username: "test")
         mace = user.characters.create(name: "Mace Windu", species: Faker::Name.name, specialization: Faker::Name.name, career: Faker::Name.name, age: Faker::Number.number(digits: 2), height: Faker::Name.name, build: Faker::Name.name, hair: Faker::Name.name, eyes: Faker::Name.name)
         vader = user.characters.create(name: "Darth Vader", species: Faker::Name.name, specialization: Faker::Name.name, career: Faker::Name.name, age: Faker::Number.number(digits: 2), height: Faker::Name.name, build: Faker::Name.name, hair: Faker::Name.name, eyes: Faker::Name.name)
-        mace_characteristics = mace.obligations.create(ob_type: 'dutybound', magnitude: 10)
-        vader_characteristics = vader.obligations.create(ob_type: 'betrayal', magnitude: 10)
+        mace_obligations = mace.obligations.create(ob_type: 'dutybound', magnitude: 10)
+        vader_obligations = vader.obligations.create(ob_type: 'betrayal', magnitude: 10)
 
         query = <<~GQL
                   query {
