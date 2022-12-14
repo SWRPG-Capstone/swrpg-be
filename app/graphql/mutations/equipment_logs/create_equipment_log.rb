7089a664fe62dda37be3fea350e1385467fa1bf1
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2022-12-13 18:10:20 -0700

    Update equipment_log mutation

--- app/graphql/mutations/equipment_logs/create_equipment_log.rb
+++ app/graphql/mutations/equipment_logs/create_equipment_log.rb
@@ -5,12 +5,11 @@ module Mutations
       argument :armor, String, required: true
       argument :credits, String, required: true
       argument :personal_gear, String, required: true
-      argument :weapons, String, required: true
 
       type Types::EquipmentLogType
 
       def resolve(character_id:, **attributes)
-        Character.find(character_id).equipment_logs.create!(attributes)
+        Character.find(character_id).create_equipment_log(attributes)
       end
     end
   end
