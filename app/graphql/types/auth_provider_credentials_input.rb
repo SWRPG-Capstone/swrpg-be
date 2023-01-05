b9e14601a77e365f7c72058355debe29fe37ef20
Suzie K <suziekim.dev@gmail.com>
2023-01-05 11:55:19 -0500

    [Fix] Update AuthProviderCredentialsInput

--- app/graphql/types/auth_provider_credentials_input.rb
+++ app/graphql/types/auth_provider_credentials_input.rb
@@ -2,7 +2,7 @@ module Types
   class AuthProviderCredentialsInput < BaseInputObject
     graphql_name 'AUTH_PROVIDER_CREDENTIALS'
 
-    argument :email, String, required: true
+    argument :username, String, required: true
     argument :password, String, required: true
   end
 end
